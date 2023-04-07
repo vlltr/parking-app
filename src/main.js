339151cceb819e6af37c82fa545e01254b20b851
Mauricio Villatoro <mauripr26@gmail.com>
2023-04-07 16:45:32 -0600

    add show validation error in register view

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
+import '@/bootstrap.js'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import ValidationError from "@/components/ValidationError.vue";
 
 import './assets/main.css'
+
+
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.component("ValidationError", ValidationError);
 
 app.mount('#app')
--- src/views/Auth/RegisterView.vue
+++ src/views/Auth/RegisterView.vue
@@ -1,29 +1,43 @@
+<script setup>
+import { onBeforeUnmount } from "vue";
+import { useRegister } from "@/stores/register";
+
+const store = useRegister();
+
+onBeforeUnmount(store.resetForm);
+</script>
+ 
 <template>
-    <form @submit.prevent="() => { }" novalidate>
+    <form @submit.prevent="store.handleSubmit" novalidate>
         <div class="flex flex-col mx-auto md:w-96 w-full">
             <h1 class="text-2xl font-bold mb-4 text-center">Register</h1>
             <div class="flex flex-col gap-2 mb-4">
                 <label for="name" class="required">Name</label>
-                <input id="name" name="name" type="text" class="form-input" autocomplete="name" required />
+                <input v-model="store.form.name" id="name" name="name" type="text" class="form-input" autocomplete="name"
+                    required />
+                <ValidationError :errors="store.errors" field="name" />
             </div>
 
             <div class="flex flex-col gap-2 mb-4">
                 <label for="email" class="required">Email</label>
-                <input id="email" name="email" type="email" class="form-input" autocomplete="email" required />
+                <input v-model="store.form.email" id="email" name="email" type="email" class="form-input"
+                    autocomplete="email" required />
+                <ValidationError :errors="store.errors" field="email" />
             </div>
 
             <div class="flex flex-col gap-2 mb-4">
                 <label for="password" class="required">Password</label>
-                <input id="password" name="password" type="password" class="form-input" autocomplete="new-password"
-                    required />
+                <input v-model="store.form.password" id="password" name="password" type="password" class="form-input"
+                    autocomplete="new-password" required />
+                <ValidationError :errors="store.errors" field="password" />
             </div>
 
             <div class="flex flex-col gap-2">
                 <label for="password_confirmation" class="required">
                     Confirm password
                 </label>
-                <input id="password_confirmation" name="password_confirmation" type="password" class="form-input"
-                    autocomplete="new-password" required />
+                <input v-model="store.form.password_confirmation" id="password_confirmation" name="password_confirmation"
+                    type="password" class="form-input" autocomplete="new-password" required />
             </div>
 
             <div class="border-t h-[1px] my-6"></div>
